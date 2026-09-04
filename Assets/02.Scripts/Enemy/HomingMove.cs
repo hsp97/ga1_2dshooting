2f8fd94fe4157cf01f7316505626ef6216b2d84d
박현서 <48700741+hsp97@users.noreply.github.com>
2026-09-04 10:42:44 +0900

    refactor: 캐싱기법

--- Assets/02.Scripts/Enemy/HomingMove.cs
+++ Assets/02.Scripts/Enemy/HomingMove.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class HomingMove : Enemy
 {
+    // 캐싱 기법
     private GameObject _player;
     private Vector2 _direction;
     [SerializeField]
     private float _moveSpeed;
     private void Start()
     {
+        _player = GameObject.FindWithTag("Player");
     }
     protected override void Move()
     {
-        _player = GameObject.FindWithTag("Player");
         _direction = _player.transform.position - transform.position;
         Vector2 normalizedSpeed = _direction.normalized * _moveSpeed;
         transform.Translate(normalizedSpeed * Time.deltaTime);
