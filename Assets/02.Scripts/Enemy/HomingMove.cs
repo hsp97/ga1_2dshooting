dd21b1c4d0ddf39e697b63b97b1788f40ef7d5a8
박현서 <48700741+hsp97@users.noreply.github.com>
2026-09-04 10:26:40 +0900

    fix: 테스트를 위한 findwithtag 위치 변경

--- Assets/02.Scripts/Enemy/HomingMove.cs
+++ Assets/02.Scripts/Enemy/HomingMove.cs
@@ -8,10 +8,10 @@ public class HomingMove : Enemy
     private float _moveSpeed;
     private void Start()
     {
-        _player = GameObject.FindWithTag("Player");
     }
     protected override void Move()
     {
+        _player = GameObject.FindWithTag("Player");
         _direction = _player.transform.position - transform.position;
         Vector2 normalizedSpeed = _direction.normalized * _moveSpeed;
         transform.Translate(normalizedSpeed * Time.deltaTime);
