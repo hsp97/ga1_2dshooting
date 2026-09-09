a6660544ab7aedbea46f71ead6e6980b7db0425c
박현서 <48700741+hsp97@users.noreply.github.com>
2026-09-09 12:59:02 +0900

    feat: 백그라운드 스프라이트 추가

--- Assets/02.Scripts/Item/BezierMove.cs
+++ Assets/02.Scripts/Item/BezierMove.cs
@@ -1,7 +1,7 @@
 using Unity.Mathematics;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class BezierMove
 {
     private Transform _target;
     private Vector3 _p1, _p2, _p3;
@@ -13,12 +13,21 @@ public class TestScript : MonoBehaviour
         _p2 = P2.transform.position;
         _p3 = P3.transform.position;
     }
-
     private void Update()
     {
         _time += Time.deltaTime;
         Vector3 p4 = Vector3.Lerp(_p1, _p2, _time / 10);
         Vector3 p5 = Vector3.Lerp(_p2, _p3, _time / 10);
-        transform.position = Vector3.Lerp(p4, p5, _time / 10);
+        //transform.position = Vector3.Lerp(p4, p5, _time / 10);
+    }
+    private void CalculateMove(GameObject g1, GameObject g2)
+    {
+        Vector3 p1 = g1.transform.position;
+        Vector3 p2 = g2.transform.position;
+        Vector3 p3 = new Vector3(0, 0, 0);
+    }
+    public static void OnMoveBezier(GameObject g1, GameObject g2)
+    {
+        //CalculateMove(g1, g2);
     }
 }
