f3a516b45ef454655196db258ccbd66be8c3cdbc
박현서 <48700741+hsp97@users.noreply.github.com>
2026-09-10 14:46:38 +0900

    feat: 데이터 저장, 로드(PlayerPrefs 이용)
    - 베스트 스코어를 컴퓨터 내부에 저장

--- Assets/02.Scripts/ScoreManager.cs
+++ Assets/02.Scripts/ScoreManager.cs
@@ -16,7 +16,7 @@ public class ScoreManager : MonoBehaviour
     // UI 책임 추가 (텍스트메시 프로 참조)
     [SerializeField] private TextMeshProUGUI _bestScoreTextUI;
     [SerializeField] private TextMeshProUGUI _currentScoreTextUI;
-
+    private const string SaveKey = "BestScore";
     private void Awake()
     {
         // 늦게 생성된 매니저는 삭제 (단일성 보장)
@@ -26,6 +26,17 @@ public class ScoreManager : MonoBehaviour
             return;
         }
         _instance = this;
+    }
+
+    public void Start()
+    {
+        // 입력: Input.
+        // 저장/불러오기: PlayerPrefs
+        if (PlayerPrefs.HasKey(SaveKey))
+        {
+            _bestScore = PlayerPrefs.GetInt(SaveKey);
+        }
+        _bestScore = PlayerPrefs.GetInt(SaveKey, 0);
         Refresh();
     }
     public void AddScore(int score)
@@ -35,6 +46,10 @@ public class ScoreManager : MonoBehaviour
         if (_currentScore > _bestScore)
         {
             _bestScore = _currentScore;
+            // 저장: PlayerPrefs.Set~ 시리즈를 이용해서 int/float/string 을 저장가능하다.
+            // 내 컴퓨터 어딘가에 저장이 된다.
+            PlayerPrefs.SetInt(SaveKey, _bestScore);
+            PlayerPrefs.Save();
         }
         Refresh();
     }
