79137e79fc40b82e976b64be24e980db615919dd
박현서 <48700741+hsp97@users.noreply.github.com>
2026-09-10 11:04:55 +0900

    refactor: auto move 리팩토링

--- Assets/02.Scripts/Player/PlayerAutoMove.cs
+++ Assets/02.Scripts/Player/PlayerAutoMove.cs
@@ -3,47 +3,68 @@ using UnityEngine;
 public class PlayerAutoMove : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    private Vector3 _position;
-    private Vector3 _enemyPosition;
-    private float _hypo = 9999;
-    private float _distanceX;
-    private float _distanceY;
+    [SerializeField] private int _stopTrackingY = 2;
+
+    private GameObject _target = null;
 
     private void Update()
     {
-        _position = transform.position;
-        _position.x = transform.position.x;
-        _position.y = transform.position.y;
-        _enemyPosition.y = _enemyPosition.y - 1f;
-        Vector3 direction = _enemyPosition - transform.position;
-        Vector3 normalizedSpeed = direction.normalized * _speed;
-        transform.position += normalizedSpeed * Time.deltaTime;
-    }
-    private void FixedUpdate()
-    {
-        TrackEnemy();
-    }
-    private void TrackEnemy()
-    {
-        GameObject[] enemys = GameObject.FindGameObjectsWithTag("Enemy");
-        if (enemys.Length == 0)
+        if (_target == null || _target.transform.position.y < -_stopTrackingY)
         {
-            return;
+            FindNearestTarget();
         }
-        float enemyHypo = 0;
-        _hypo = 9999;
-        foreach (GameObject enemy in enemys)
+
+        Move();
+    }
+
+    private void Move()
+    {
+        if (_target == null) return;
+
+        // 2. 방향을 구한다.
+        Vector3 diff = _target.transform.position - transform.position;
+        Vector3 direction = diff;
+
+        // 적과 나와의 y축 차이가 3보다 크면 앞으로 가고 아니라면 뒤로가게
+        if (diff.y >= 3)
         {
-            Vector3 enemyPosition = enemy.transform.position;
-            //Debug.Log($"적의 좌표({enemyPosition.x},{enemyPosition.y})");
-            _distanceX = _position.x - enemyPosition.x;
-            _distanceY = _position.y - enemyPosition.y;
-            // 빗변 같은 짓 안해도 거리 구하는 함수가 있다.
-            // Vector2.Distance(transform.position, enemyPosition);
-            if (_hypo > _distanceX * _distanceX + _distanceY * _distanceY)
+            direction.y = 1;
+        }
+        else
+        {
+            direction.y = -1;
+        }
+
+        direction.Normalize();
+
+        // 3. 속도에 맞게 이동을한다.
+        transform.position += direction * _speed * Time.deltaTime;
+    }
+
+    private void FindNearestTarget()
+    {
+        // 1. 타겟을 구한다.
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
+        if (targets.Length == 0) return;
+
+        _target = targets[0];
+        float minDistance = float.MaxValue;
+
+        // 1-1. 가장 가까운 타겟을 찾는다.
+        foreach (GameObject enemy in targets)
+        {
+            if (enemy.transform.position.y < -_stopTrackingY)
             {
-                _hypo = _distanceX * _distanceX + _distanceY * _distanceY;
-                _enemyPosition = enemyPosition;
+                continue;
+            }
+
+            // 거리를 구해서
+            float distance = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distance < minDistance) // 저장된 거리보다 짧다면
+            {
+                // 타겟 변경
+                minDistance = distance;
+                _target = enemy;
             }
         }
     }
