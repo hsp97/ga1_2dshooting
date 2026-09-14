cc190dd187d99fe71d32c8dbbbe6fda3b886c888
박현서 <48700741+hsp97@users.noreply.github.com>
2026-09-14 12:35:21 +0900

    chore: 점수 표기 숫자 3자리마ㅏ 쉼표
    - 문자열 서식 N0적용

--- Assets/02.Scripts/UI/UI_AutoButton.cs
+++ Assets/02.Scripts/UI/UI_AutoButton.cs
@@ -10,21 +10,11 @@ public class UI_AutoButton : MonoBehaviour
     [Header("on/off 스프라이트")]
     [SerializeField] private Sprite _onSprite;
     [SerializeField] private Sprite _offSprite;
-    [SerializeField] private AudioSource _audioSource;
 
     private Image _myImage;
     private bool _autoMode = false;
     private Player _player;
     private PlayerFire _playerFire;
-
-    [Header("클릭시 애니메이션")]
-    [SerializeField] private AnimationCurve _bumpCurve;
-
-    private float _scale = 1.0f;
-    private bool _isBumping = false;
-    private float _elapsedTime = 0; // 경과시간
-    private const float BumpDuration = 0.1f;
-    private const float BumpScale = 1.1f;
 
     private void Start()
     {
@@ -46,36 +36,7 @@ public class UI_AutoButton : MonoBehaviour
 
         // todo: 버튼 클릭할 때 애니메이션 추가 + 사운드 추가
         // 애니메이션: 코드로 구현 약간 커졌다가 작아지게
+        // 애니메이션 컴포넌트: UI_ButtonClick
         // 사운드: 일레븐랩스에서 버튼 클릭 공용 사운드 만들어서 적용
-    }
-
-    public void PlayAnimation()
-    {
-        _isBumping = true;
-        _elapsedTime = 0;
-    }
-
-    private void Update()
-    {
-        if (!_isBumping) return;
-
-        // 경과 시간 누적
-        _elapsedTime += Time.deltaTime;
-        if (_elapsedTime > BumpDuration)
-        {
-            transform.localScale = Vector3.one;
-            _isBumping = false;
-            return;
-        }
-
-        // 누적 시간과 애니메이션 커브에 따른 스케일 변경
-        float time = _elapsedTime / BumpDuration; // 얼마나 지났는지 퍼센트 0~1
-        float curveValue = _bumpCurve.Evaluate(time); // 퍼센트에 따라 커브 애니메이션 값 추출
-        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * BumpScale, curveValue);
-    }
-
-    public void PlaySound()
-    {
-        _audioSource.Play();
     }
 }
